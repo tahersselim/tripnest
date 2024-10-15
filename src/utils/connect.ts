14f49d0387e38adc20cc178ac083901fe8c30930
tahersselim <taher.selim.dev@gmail.com>
2024-10-15 08:37:04 +0300

    try

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,15 +1,22 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
+// Singleton pattern to avoid multiple PrismaClient instances
 const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL, // Use DATABASE_URL from environment variables
+      },
+    },
+  });
+};
 
 declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default prisma
+export default prisma;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
